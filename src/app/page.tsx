15d5591322af5ca62e121b7e3c990e352af87d9c
Rajagopal <rajagopal@balcony.com>
2025-02-16 22:43:29 +0530

    Revert "page.tsx changed"
    This reverts commit 294bd8420d2f06b83624f9d87867bbb252f42f57.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
             <div className="grid md:grid-cols-2 gap-12 items-center">
               <div className="text-left space-y-6 ">
                 <h1 className="text-5xl font-bold bg-gradient-to-r from-cyan-500 to-blue-600 bg-clip-text text-transparent">
-                  {/* Experience Dental Excellence with a Gentle Touch */}
+                  Experience Dental Excellence with a Gentle Touch
                 </h1>
                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal</p>
                 <motion.button
